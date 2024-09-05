0c925e1535b6081d0fcf1616ad49935b86a0f10b
kleiropskuur <119229700+kleiropskuur@users.noreply.github.com>
2024-09-05 17:17:20 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.menubtn').onclick = function (e) {
-  var menubtn = document.querySelector('.menubtn');
-  var menu = document.querySelector('.menu');
-
-  menubtn.classList.toggle('is-active');
-  menu.classList.toggle('is-active');
-
-  e.preventDefault();
-}
